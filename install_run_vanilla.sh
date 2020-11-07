b5646238be6c5e384fc46ede57de372653c71883
kitschbent <34662130+kitschbent@users.noreply.github.com>
2020-11-07 10:36:41 -0500

    Update install_run_vanilla.sh

--- install_run_vanilla.sh
+++ install_run_vanilla.sh
@@ -2,8 +2,9 @@
 
 apt-get update && apt-get upgrade -y
 apt install unzip
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_12.x
+sudo bash -
+apt-get install -y nodejs
 wget https://github.com/hoprnet/hopr-chat/releases/download/v1.17.14-basodino-testnet/hopr-chat-nodebin-linux.zip
 unzip hopr-chat-nodebin-linux.zip
 rm -f hopr-chat-nodebin-linux.zip
